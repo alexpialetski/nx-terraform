ff589369d914e2cfd807bcb3f88c44a952c6bdfa
alexpialetski <alexcoolpel@gmail.com>
2025-10-29 18:01:00 +0300

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/AYYegg45uL

---
+++ packages/create-nx-terraform-app/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'create-nx-terraform-app',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/packages/create-nx-terraform-app',
+};
